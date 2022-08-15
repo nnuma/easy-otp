04a8c56add97e7e59cd79990cc365833f47ab1ef
Kensuke Naganuma <kensuke.naganuma@gmail.com>
2022-08-15 12:04:54 +0900

    fix: Lacking a number of minutes when a minutes in 0 to 9

--- src/otp.rs
+++ src/otp.rs
@@ -25,12 +25,13 @@ fn get_current_datetime_str(config: &Config) -> (String, i64) {
   let now: DateTime<Local> = Local::now();
   let minutes = now.timestamp_millis() / 1000 / 60 % 60;
   let interval_minutes = config.interval_minutes;
-  let minutes_str = format!("{00}", interval_minutes * (minutes / interval_minutes));
-  let datetime_str = now.format("%Y%m%d%H");
-  let targ_datetime_str = now.format("%Y-%m-%dT%H:{}:00%:z").to_string().replace("{}", &format!("{:0>2}", minutes_str)).to_string();
+  let minutes_str = format!("{:0>2}", interval_minutes * (minutes / interval_minutes));
+  let datetime_str = format!("{}{}", now.format("%Y%m%d%H").to_string(), minutes_str);
+  println!("{}", datetime_str);
+  let targ_datetime_str = now.format("%Y-%m-%dT%H:{}:00%:z").to_string().replace("{}", minutes_str.as_str()).to_string();
   let targ_datetime = DateTime::parse_from_rfc3339(&targ_datetime_str).unwrap();
   let diff = now.timestamp_millis() - targ_datetime.timestamp_millis();
   let remaining_seconds = (60 * config.interval_minutes) - (diff / 1000);
 
-  return (format!("{}{}", datetime_str.to_string(), minutes_str), remaining_seconds);
+  return (datetime_str, remaining_seconds);
 }
